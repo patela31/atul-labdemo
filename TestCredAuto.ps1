3725f6f7c02975176d14ffa4dd85130257f89f46
patela31 <67654451+patela31@users.noreply.github.com>
2020-08-22 14:04:47 +0100

    Add files via upload

---
+++ TestCredAuto.ps1
@@ -0,0 +1,60 @@
+﻿Workflow Stop-Start-AzureVM 
+{ 
+    Param 
+    (    
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureSubscriptionId, 
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureVMList="All", 
+        [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")] 
+        [String] 
+        $Action 
+    ) 
+     
+    $credential = Get-AutomationPSCredential -Name 'atul-testcred' 
+     Write-Output "Checkpoint: [$credential]"
+   
+    $null = Add-AzureRmAccount -Credential $Cred -ErrorAction Stop
+	$SubId = Get-AutomationVariable -Name 'AzureSubscriptionID'
+	$null = Set-AzureRmContext -SubscriptionId $SubId -ErrorAction Stop 
+   
+ 
+    if($AzureVMList -ne "All") 
+    { 
+        $AzureVMs = $AzureVMList.Split(",") 
+        [System.Collections.ArrayList]$AzureVMsToHandle = $AzureVMs 
+    } 
+    else 
+    { 
+        $AzureVMs = (Get-AzVM).Name 
+        [System.Collections.ArrayList]$AzureVMsToHandle = $AzureVMs 
+ 
+    } 
+ 
+    foreach($AzureVM in $AzureVMsToHandle) 
+    { 
+        if(!(Get-AzVM | ? {$_.Name -eq $AzureVM})) 
+        { 
+            throw " AzureVM : [$AzureVM] - Does not exist! - Check your inputs " 
+        } 
+    } 
+ 
+    if($Action -eq "Stop") 
+    { 
+        Write-Output "Stopping VMs"; 
+        foreach -parallel ($AzureVM in $AzureVMsToHandle) 
+        { 
+            Get-AzVM | ? {$_.Name -eq $AzureVM} | Stop-AzVM -Force 
+        } 
+    } 
+    else 
+    { 
+        Write-Output "Starting VMs"; 
+        foreach -parallel ($AzureVM in $AzureVMsToHandle) 
+        { 
+            Get-AzVM | ? {$_.Name -eq $AzureVM} | Start-AzVM 
+        } 
+    } 
+}
